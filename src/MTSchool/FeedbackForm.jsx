4facc55a17bfd96694c77ba70f3725cbf7911319
vadymSarvyk <96552273+vadymSarvyk@users.noreply.github.com>
2024-01-17 20:33:43 +0200

    Add files via upload

---
+++ src/MTSchool/FeedbackForm.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './FeedbackForm.css';
+
+function FeedbackForm() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [message, setMessage] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    function validateForm() {
+        return email.length > 0 && name.length > 0 && phone.length > 0 && message.length > 0;
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        if (!validateForm()) {
+            alert('Please fill in all fields.');
+            return;
+        }
+
+        setLoading(true);
+
+        const formData = new FormData();
+        formData.append('Name', name);
+        formData.append('Email', email);
+        formData.append('Phone', phone);
+        formData.append('Message', message);
+
+        axios.post("https://script.google.com/macros/s/AKfycbx3E67yE8S0BRYMY2gG2nKyR7wGCMk0h9wBpMNcQAqTMYlE1R1toTEYdxGREEvCflZO/exec", formData)
+        .then(response => {
+          setLoading(false);
+          
+        })
+        .catch(error => {
+          setLoading(false);
+        });
+    }
+
+    return (
+        <div className="form">
+          {/* Прелоадер */}
+          {loading && <div className="overlay">
+            <div className="loader"></div>
+          </div>}
+
+          <div className="form-container">
+            <h1 className="form-header">Напишіть нам і ми Вам зателефонуємо!</h1>
+            <h2 className="form-subheader">Як до Вас звертатися</h2>
+            <form className="form" onSubmit={handleSubmit}>
+              <input 
+                placeholder="Ваше ім'я" 
+                value={name}
+                onChange={e => setName(e.target.value)}
+                type="text" 
+              />
+              <input 
+                placeholder="Ваш e-mail" 
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                type="email"
+              />
+              <input 
+                placeholder="Номер телефону" 
+                value={phone}
+                onChange={e => setPhone(e.target.value)}
+                type="tel"
+              />
+              <textarea 
+                placeholder="Коротко опишіть Ваші побажання до курсу"
+                value={message}
+                onChange={e => setMessage(e.target.value)}
+              />
+              <input type="submit" value="Відправити" />
+            </form>
+          </div>
+        </div>
+      );
+}
+
+export default FeedbackForm
